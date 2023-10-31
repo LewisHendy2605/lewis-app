16998e85032ad0649312be9ee3d74bd56a8d73ac
LewisHendy2605 <lewishendy26@gmail.com>
2023-10-31 17:13:44 +0000

    Added UserTableseeder + added Forigkey functions

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function car()
+    {
+        return $this->hasOne(Car::class);
+    }
+
+
 }
