8b845a346d82b163b0bc8fd39453beb7eb1d4496
LewisHendy2605 <lewishendy26@gmail.com>
2023-10-31 15:44:50 +0000

    Completed review migration

--- database/migrations/2023_10_31_152927_create_reviews_table.php
+++ database/migrations/2023_10_31_152927_create_reviews_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->string('car_manufacture');
+            $table->string('car_model');
+            $table->integer('stars');
+            $table->string('comments');
             $table->timestamps();
         });
     }
