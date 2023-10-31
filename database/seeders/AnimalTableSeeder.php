16fe1362eb87a3cf257d2b3515b960da0a7d0359
LewisHendy2605 <lewishendy26@gmail.com>
2023-10-31 12:34:54 +0000

    Added a single hard coded entry to animal seeder

--- database/seeders/AnimalTableSeeder.php
+++ database/seeders/AnimalTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Animal;
 
 class AnimalTableSeeder extends Seeder
 {
@@ -13,5 +14,9 @@ class AnimalTableSeeder extends Seeder
     public function run(): void
     {
         //
+        $a = new Animal;
+        $a->name = "Not G";
+        $a->weight = 400.0;
+        $a->save();
     }
 }
