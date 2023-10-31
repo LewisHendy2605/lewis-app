03aa7343c98b237e1a41a02f50b7549db140dad4
LewisHendy2605 <lewishendy26@gmail.com>
2023-10-31 12:18:59 +0000

    Addedd animal seeder

--- database/migrations/2023_10_30_193142_create_animals_table.php
+++ database/migrations/2023_10_30_193142_create_animals_table.php
@@ -20,11 +20,13 @@ return new class extends Migration
         });
     }
 
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
         Schema::dropIfExists('animals');
+        
     }
 };
