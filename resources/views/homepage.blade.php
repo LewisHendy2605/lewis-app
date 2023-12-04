7d4bb990b1737122dee3dc6242d3247c30034855
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-04 11:27:45 +0000

    Reorganise code+ added search car LWC

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,24 +4,21 @@
 
 @section('content')
 
-<body class="antialiased">
-        
-        <div style="text-align: left">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <h4><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></h4>
+    <div style="text-align: left">
+        @if (Route::has('login'))
+            <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                @else
+                    <h4><a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></h4>
+                @if (Route::has('register'))
+                    <h4><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></h4>
+                @endif
+                @endauth
+            </div>
+        @endif
+    </div>
 
-                        @if (Route::has('register'))
-                            <h4><a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a></h4>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div>
-    </body>
 
     <div style="text-align: center">
         <h2><a href="{{ route('cars.index')}}">Cars</a></h2>
--- resources/views/livewire/show-user-reviews.blade.php
+++ resources/views/livewire/show-user-reviews.blade.php
@@ -1,17 +1,18 @@
 <div>
-<ul> 
-    @foreach ($reviews as $review)
-        <li><a href="{{route('reviews.show', ['id' => $review->id])}}" >
-            Review ID: {{$review->id}}</a></li> 
-        @foreach ($cars as $car)
-            @if ($car->id == $review->car_id)
-                <li><a href="{{route('cars.show', ['id' => $car->id])}}">
-                    Car: {{$car->manufacture}}, {{$car->model}}, ID: {{$car->id}}</a></li>
-            @endif
-        @endforeach  
-        <li>Stars: {{$review->stars}}</li>
-        <li>Comment: {{$review->comment}}</li>
-        <p></p>
-    @endforeach
-</ul>
+    {{}}
+    <ul> 
+        @foreach ($reviews as $review)
+            <li><a href="{{route('reviews.show', ['id' => $review->id])}}" >
+                Review ID: {{$review->id}}</a></li> 
+            @foreach ($cars as $car)
+                @if ($car->id == $review->car_id)
+                    <li><a href="{{route('cars.show', ['id' => $car->id])}}">
+                        Car: {{$car->manufacture}}, {{$car->model}}, ID: {{$car->id}}</a></li>
+                @endif
+            @endforeach  
+            <li>Stars: {{$review->stars}}</li>
+            <li>Comment: {{$review->comment}}</li>
+            <p></p>
+        @endforeach
+    </ul>
 </div>
