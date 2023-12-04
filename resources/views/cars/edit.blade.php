d39c20260a9aecbe4e882591fd7851027466b87c
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-04 15:38:38 +0000

    Added year field to car edit view

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<form method="post" action="{{ route('cars.update', ['id' => $id]) }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('cars.update', ['id' => $car->id]) }}" class="mt-6 space-y-6">
         @csrf
         @method('patch')
 
@@ -24,10 +24,18 @@
             <x-input-label for="model" :value="__('Model')" />
             <x-text-input id="model" name="model" type="text" class="mt-1 block w-full" :value="old('model', $car->model)" required autocomplete="model" />
             <x-input-error class="mt-2" :messages="$errors->get('model')" />
+        </div>
 
-            
+        <div>
+            <x-input-label for="year" :value="__('Year')" />
+            <x-text-input id="year" name="year" type="text" class="mt-1 block w-full" :value="old('year', $car->year)" required autofocus autocomplete="year" />
+            <x-input-error class="mt-2" :messages="$errors->get('year')" />
         </div>
 
     </form>
+
+    <h2><a href="{{ route('cars.show', ['id' => $car->id])}}">Back</a></h2>
+
+
 
 @endsection
