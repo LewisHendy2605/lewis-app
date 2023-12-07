cf787de9f5c2a25c19e8f31bea9f8b5018dbaa88
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-07 22:25:05 +0000

    Added show review comments LWC

---
+++ app/Livewire/ShowReviewComments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowReviewComments extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-review-comments');
+    }
+}
