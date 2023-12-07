79a626b0a5feedd11d6d1fa69724e786d2301013
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-07 21:16:46 +0000

    Added create review comment LWC

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -5,7 +5,21 @@
 @section('content')
     <h2><a href="{{ route('home')}}">Home</a></h2>
     <h3>{{$reviews->count()}} reviews</h3>
-    <button><a href="{{ route('reviews.create')}}">Create a Review</a></button>
+
+    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+        @auth
+            <button><a href="{{ route('reviews.create')}}">Create a Review</a></button>
+        @else
+            <h4>You need to log in to create a review</h4>
+            @if (Route::has('login'))
+                <h4><a href="{{ route('login') }}">Log in</a></h4>
+                @if (Route::has('register'))
+                    <h4><a href="{{ route('register') }}">Register</a></h4>
+                @endif
+            @endif
+        @endauth
+<   /div>
+
 
     <livewire:searchReviews/>
 @endsection
