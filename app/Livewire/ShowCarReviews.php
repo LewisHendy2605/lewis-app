a6c78b0d48ac8278a0d16e9c5ff8afff6d4ec51c
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-09 13:19:22 +0000

    created show car reviews LWC

---
+++ app/Livewire/ShowCarReviews.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ShowCarReviews extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-car-reviews');
+    }
+}
