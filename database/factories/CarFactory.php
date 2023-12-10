433af7959fc0e7da53d4a1d070594acb307a43e1
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-10 14:46:18 +0000

    Updated car table to store image name + factory

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -24,13 +24,18 @@ class CarFactory extends Factory
         $mod = ["GT86", "Yaris", "Testarossa", "Focus"];
 
         $year = ["1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000",
-    "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010". "2011", "2012", "2013", "2014",
-"2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"];
+                    "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010". "2011", "2012", "2013", "2014",
+                    "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"];
+
+        $img = ["audi_suv", "toyota_gt_white", "toyota_gt_gray", "toyota_gt_black", "toyota_gt_blue",
+         "toyota_gt_rear", "toyota_yaris", "toyota_yaris_blue", "toyota_yaris_white",
+          "vauxhaul_corsa", "vauxhaul_corsa_blue", "jaguar_etype"];
 
         return [
             'manufacture' => $this->faker->randomElement($man),
             'model' => $this->faker->randomElement($mod),
-            'year' => $this->faker->randomElement($year)
+            'year' => $this->faker->randomElement($year),
+            'image_name' => $this->faker->randomElement($img)
         ];
     }
 }
--- database/migrations/2023_10_31_151733_create_cars_table.php
+++ database/migrations/2023_10_31_151733_create_cars_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('manufacture');
             $table->string('model');
             $table->string('year');
+            $table->string('image_name');
             $table->timestamps();
         });
     }
