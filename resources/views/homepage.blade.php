0652e15f3f31352aa0721e32c17eadef9569c9a8
LewisHendy2605 <lewishendy26@gmail.com>
2023-12-10 14:34:02 +0000

    Added images file with all images+fixed images

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.appp')
 
-@section('title', "Lewis' Car Review's")
+@section('title', "The Car Review site")
 
 @section('content')
 
 
-    <img src="pic_trulli.jpg" alt="Trulli" width="500" height="333"/>
-    <img src="{{ asset('pic_trulli.jpg') }}" alt="tag"/>
-    {!! Html::image('pic_trulli.jpg') !!}
-    
+<img src="images/pic_trulli.jpg" alt="Trulli" width="500" height="333"/>
+    <img src="{{ asset('images/pic_trulli.jpg') }}" alt="tag"/>
+    {!! Html::image('images/pic_trulli.jpg') !!}
+    <img src="{{URL::to('images/pic_trulli.jpg')}}" alt="" width="500" height="333"/>
+    <div>
+        <h1>Welcome to The Car Review site !!</h1>
+        <h3>The best car reviews online</h3>
+    </div>
     
     @endsection
